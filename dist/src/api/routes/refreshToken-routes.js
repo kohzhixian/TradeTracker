3c531b89eecdd65f17b5584aa4ac89d595a4694c
kohzhixian <zhixianwork@gmail.com>
2023-07-08 17:35:56 +0800

    delete dist folder

--- dist/src/api/routes/refreshToken-routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// const express = require("express");
-// const Router = express.Router();
-// const refreshTokenController = require("../controllers/refreshToken-controller");
-// Router.get('/getTokenByUserId', refreshTokenController.getTokenByUserId);
-// Router.delete('/logout/:userId', refreshTokenController.logout);
-// module.exports = Router;
